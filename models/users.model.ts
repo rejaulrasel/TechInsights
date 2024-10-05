070af04a56ef4740ea7b00dee0538beb3102d518
rejaulrasel <rejaulhasanrasel28@gmail.com>
2024-10-06 03:07:34 +0600

    edit user creation logic

--- models/users.model.ts
+++ models/users.model.ts
@@ -11,15 +11,24 @@ const userSchema = new Schema<IUser>({
     isPremiumMember: { type: Boolean, required: true, default: false },
 
     role: { type: String, enum: ['admin', 'user'], required: true, default: 'user' },
+    followers: [{ type: Schema.Types.ObjectId, ref: 'User' }],  // Array of User IDs for followers
+    following: [{ type: Schema.Types.ObjectId, ref: 'User' }],  // Array of User IDs for following
 }, {
     timestamps: true,
 });
 
+// Hash the password before saving the user
 
 userSchema.pre('save', async function (next) {
-    this.password = await bcrypt.hash(this.password, Number(process.env.BCRYPT_SALT_ROUNDS))
-    next()
-})
+    if (this.isModified('password')) {
+        this.password = await bcrypt.hash(this.password, Number(process.env.BCRYPT_SALT_ROUNDS));
+    }
+    next();
+});
+// Method to populate followers and following fields when getting user data
+userSchema.methods.populateFollowersAndFollowing = async function () {
+    return await this.populate('followers following').execPopulate();
+};
 
 
 const User = mongoose.models.User || mongoose.model<IUser>('User', userSchema);
