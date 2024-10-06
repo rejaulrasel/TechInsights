ecbd0aa73fd5be18e289be878a11c03c7f0a0ccb
rejaulrasel <rejaulhasanrasel28@gmail.com>
2024-10-06 15:25:30 +0600

    added premium members verification badge

---
+++ components/premium_acc_badge.tsx
@@ -0,0 +1,17 @@
+"use client";
+import { VerifiedBadge } from "./icons";
+const UserName = ({
+  name,
+  isPremium,
+}: {
+  name: string | undefined;
+  isPremium: boolean | undefined;
+}) => {
+  return (
+    <div className="flex items-center">
+      <h1>{name}</h1>
+      {isPremium && <VerifiedBadge />}
+    </div>
+  );
+};
+export default UserName;
