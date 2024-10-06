69f663f770917ea2ce4d1fc1e7fceab230eab6c4
rejaulrasel <rejaulhasanrasel28@gmail.com>
2024-10-06 15:51:34 +0600

    user registred successfull

--- app/(main_layout)/page.tsx
+++ app/(main_layout)/page.tsx
@@ -1,3 +1,3 @@
 export default function Home() {
-  return <div className="">k</div>;
+  return <div className="">Hello Home</div>;
 }
--- services/auth.services.ts
+++ services/auth.services.ts
@@ -1,13 +1,22 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 'use server'
+
 import { FieldValues } from "react-hook-form"
 
 import axiosInstance from "@/libs/axiosInstance"
+
 export const registerUser = async (userData: FieldValues) => {
+
     try {
         const { data } = await axiosInstance.post('/auth/register', userData);
         return data;
+
     } catch (error: any) {
-        throw new Error(error);
+        // Check if it's an Axios error with a response
+        if (error.response) {
+            // Extract the error message from the response
+            const errorMessage = error.response.data.message;
+            throw new Error(errorMessage);
+        }
     }
 } 
