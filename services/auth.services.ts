0070ba5566e90f29e7e54192d43632d190496911
rejaulrasel <rejaulhasanrasel28@gmail.com>
2024-10-06 15:42:38 +0600

    server action created for register user

--- services/auth.services.ts
+++ services/auth.services.ts
@@ -1,27 +1,13 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 'use server'
-
 import { FieldValues } from "react-hook-form"
 
-import axiosInstance from "@/libs/axiosInstance";
-
-
-
-
+import axiosInstance from "@/libs/axiosInstance"
 export const registerUser = async (userData: FieldValues) => {
-
     try {
         const { data } = await axiosInstance.post('/auth/register', userData);
-
         return data;
-
     } catch (error: any) {
-        // Check if it's an Axios error with a response
-        if (error.response) {
-            // Extract the error message from the response
-            const errorMessage = error.response.data.message;
-
-            throw new Error(errorMessage);
-        }
+        throw new Error(error)
     }
 } 
