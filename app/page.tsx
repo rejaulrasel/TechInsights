df7723549de01466be1bb360f989540b98467779
rejaulrasel <rejaulhasanrasel28@gmail.com>
2024-10-06 19:48:46 +0600

    articale card responcived

--- app/page.tsx
+++
@@ -1,5 +0,0 @@
-import { title } from "@/components/primitives";
-
-export default function Home() {
-  return <div className={title()}>Home</div>;
-}
--- config/site.ts
+++ config/site.ts
@@ -1,7 +1,7 @@
 export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
-    name: "Engineers IQ",
+    name: "TechInsights",
     description: "Make beautiful websites regardless of your design experience.",
     navItems: [
         {
