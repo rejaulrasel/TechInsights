d5f7697e221981f81f3fb11bf1f41441dab45dbb
rejaulrasel <rejaulhasanrasel28@gmail.com>
2024-10-07 13:42:36 +0600

    If user searches profile with wrong username then error page is shown

--- app/(main_layout)/page.tsx
+++ app/(main_layout)/page.tsx
@@ -110,8 +110,9 @@ export default function Home() {
             </div>
             <ArticlePreview
               author={{
-                name: "Alex Mathers",
-                avatar: "/placeholder.svg?height=40&width=40",
+                name: "REJAUL HASAN",
+                avatar:
+                  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSSPxliccSvodnSAgl3UaJdMWFigduYjlk0lQ&s ",
               }}
               date="2d ago"
               image="https://plus.unsplash.com/premium_photo-1685086785054-d047cdc0e525?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
