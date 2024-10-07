7eb9f077e39dd69e82ec468864503fbba529eac0
rejaulrasel <rejaulhasanrasel28@gmail.com>
2024-10-08 02:23:10 +0600

    added const files

--- const/navbar/profile_dropdown_options.ts
+++ const/navbar/profile_dropdown_options.ts
@@ -2,6 +2,7 @@ interface IProfileDropdownOptions {
     item: string;
     link: string;
 }
+
 export const profileDropdownOptions: IProfileDropdownOptions[] = [
     { item: 'Profile Settings', link: '/profile' }
 ]
