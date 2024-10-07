7fbb4fc824c26c4fca22781051f286885a384a4d
rejaulrasel <rejaulhasanrasel28@gmail.com>
2024-10-07 13:56:06 +0600

    add icons and user types

--- app/api/follow/route.ts
+++ app/api/follow/route.ts
@@ -48,6 +48,8 @@ export async function GET(request: Request) {
     }
 };
 
+
+//follow    
 export async function PATCH(request: Request) {
     const { follower, following } = await request.json();
 
@@ -104,6 +106,8 @@ export async function PATCH(request: Request) {
     }
 };
 
+
+//unfollow
 export async function DELETE(request: Request) {
     const { follower, following } = await request.json();
 
