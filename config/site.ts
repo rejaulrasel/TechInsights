74e186b735a0fd1291d87083247d5422e5824c86
rejaulrasel <rejaulhasanrasel28@gmail.com>
2024-10-09 11:15:12 +0600

    edite const and config files

--- config/site.ts
+++ config/site.ts
@@ -1,8 +1,8 @@
 export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
-    name: "Engineers IQ",
-    description: "Make beautiful websites regardless of your design experience.",
+    name: "TechInsights",
+    description: "Create stunning websites effortlessly, no matter your design expertise.",
     navItems: [
         {
             label: "Home",
---
+++ hooks/use_articles.ts
@@ -0,0 +1,60 @@
+import axios, { AxiosError } from "axios";
+import useSWR from "swr";
+
+import { IArticleResponse } from "@/interface/articles.response.interface";
+
+interface UseArticleResponse {
+    data: IArticleResponse | IArticleResponse[] | null;
+    error: AxiosError | null;
+    isLoading: boolean;
+    revalidate: () => void;
+}
+
+const useArticle = (query: {
+    category?: string;
+    author?: string;
+    searchTerm?: string;
+    isPremiumContent?: boolean;
+    topic?: string;
+    _id?: string;
+    page?: number;
+    limit?: number;
+}): UseArticleResponse => {
+    const fetcher = async (url: string): Promise<IArticleResponse | IArticleResponse[]> => {
+        const response = await axios.get(url);
+        return response.data.data;
+    };
+
+    // Build search params
+    const buildUrlWithParams = () => {
+        const params = new URLSearchParams();
+        if (query.category) params.append("category", query.category);
+
+        if (query.page) params.append("page", String(query.page));
+        if (query.limit) params.append("limit", String(query.limit));
+
+        if (query.author) params.append("author", query.author);
+        if (query.topic) params.append("topic", query.topic);
+        if (query.searchTerm) params.append("searchTerm", query.searchTerm);
+        if (query.isPremiumContent !== undefined) params.append("isPremiumContent", String(query.isPremiumContent));
+        if (query._id) params.append("_id", query._id);
+
+        return `/api/articles?${params.toString()}`;
+    };
+
+    const { data, error, isValidating, mutate } = useSWR<IArticleResponse | IArticleResponse[]>(
+        query ? buildUrlWithParams() : '/api/articles',
+        fetcher
+    );
+
+    const isLoading = isValidating;
+
+    return {
+        data: data || null,
+        error: error as AxiosError,
+        isLoading,
+        revalidate: () => mutate(),
+    };
+};
+
+export default useArticle;
