f2bbfe0389fb9486f6659866c55e56a3c882f5ca
rejaulrasel <rejaulhasanrasel28@gmail.com>
2024-10-09 11:23:59 +0600

    rename connection string

--- libs/axiosInstance.ts
+++ libs/axiosInstance.ts
@@ -1,5 +1,7 @@
 import axios from "axios";
+
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:3000/api'
+    baseURL: `${process.env.NEXT_PUBLIC_BASE_API}/api`
 });
+
 export default axiosInstance
--- libs/connect_mongodb.ts
+++ libs/connect_mongodb.ts
@@ -1,13 +1,22 @@
 import mongoose from "mongoose";
 
+let isConnected = false;
+
 const connectMongodb = async () => {
+    if (isConnected) {
+        console.log("Already connected to MongoDB.");
+        return;
+    }
+
     try {
-        // await mongoose.connect(process.env.MONGODB_URL);
-        await mongoose.connect(process.env.NEXT_PUBLIC_MONGODB_CONNECTION_STRING as string);
+        await mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string);
+        isConnected = true;
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
 
     } catch (error) {
-        console.log("There was an error connecting to MangoDB", error);
+        console.log("There was an error connecting to MongoDB", error);
+        isConnected = false;
     }
 };
 
-export default connectMongodb
+export default connectMongodb;
