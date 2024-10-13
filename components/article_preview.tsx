2a529f424060732149dcb49869085871ce1d1601
rejaulrasel <rejaulhasanrasel28@gmail.com>
2024-10-13 12:23:00 +0600

    added edit article functionality

--- components/article_preview.tsx
+++ components/article_preview.tsx
@@ -182,7 +182,9 @@ export const ArticlePreview = ({
         {fromProfile && currentUser?._id === data?.author?._id && (
           <CardFooter className="">
             <div className="flex gap-4">
-              <Chip className="">Edit </Chip>
+              <Link href={`/articles/edit/${data.author.username}/${data._id}`}>
+                <Chip className="">Edit</Chip>
+              </Link>
               <Chip
                 className="hover:cursor-pointer"
                 color="danger"
